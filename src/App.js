459af3e5fe03f3f269b713c41df526cd2ca47514
Gokul Murugesan <gokulmurugesan@192.168.1.8>
2023-11-15 19:54:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './App.css'
+
+import { BlockPicker } from 'react-color'
+import Tippy from '@tippyjs/react'
+
+function App () {
+  const [selectedColor, setSelectedColor] = useState('#ccc')
+  return (
+    <div className='app'>
+      <h1>Color Picker </h1>
+
+      <div className='area' style={{ backgroundColor: selectedColor }}>
+
+      </div>
+
+      <Tippy interactive={true} placement={'bottom'} content={
+        <BlockPicker
+          color={selectedColor}
+          onChangeComplete={color => setSelectedColor(color.hex)}
+        />
+      }>
+        <button className='ref-button'>Change box color</button>
+
+      </Tippy>
+
+    </div>
+  )
+}
+
+export default App
